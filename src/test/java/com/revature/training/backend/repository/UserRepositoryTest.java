78d8e4c7246c2945ec4337bb30fbc4040656bdea
Christopher Yang <chryang01@gmail.com>
2025-10-15 13:46:49 -0500

    Create test cases for UserRepository

---
+++ src/test/java/com/revature/training/backend/repository/UserRepositoryTest.java
@@ -0,0 +1,84 @@
+package com.revature.training.backend.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.revature.training.backend.exception.UserNotFoundException;
+import com.revature.training.backend.model.User;
+
+@DataJpaTest
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    public void testFindAllUsers() {
+        User user1 = new User("Alpha", "alpha@mail.com", "password1");
+        User user2 = new User("Bravo", "bravo@mail.com", "password2");
+        User user3 = new User("Charlie", "charlie@mail.com", "password3");
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+
+        List<User> users = userRepository.findAll();
+
+        assertNotNull(users);
+        assertEquals(3, users.size());
+    }
+
+    @Test
+    public void testSaveAndFindUser() {
+        User user = new User();
+        user.setUserName("Delta");
+        user.setEmail("delta@mail.com");
+        user.setPassword("pass1234");
+
+        userRepository.save(user);
+
+        User foundUser = userRepository.findById(user.getId()).orElseThrow();
+
+        assertNotNull(foundUser);
+        assertEquals(foundUser, user);
+    }
+
+    @Test
+    public void testExistsByEmail_EmailExists() {
+        User user = new User();
+        user.setUserName("Echo");
+        user.setEmail("echo@mail.com");
+        user.setPassword("pass1234");
+
+        userRepository.save(user);
+
+        boolean userExists = userRepository.existsByEmail("echo@mail.com");
+
+        assertEquals(userExists, true);
+    }
+
+    @Test
+    public void testExistsByEmail_EmailDoesNotExists() {
+        boolean userExists = userRepository.existsByEmail("hotel@mail.com");
+
+        assertEquals(userExists, false);
+    }
+
+    @Test
+    public void testDeleteUser() {
+        User user = new User();
+        user.setUserName("Foxtrot");
+        user.setEmail("foxtrot@mail.com");
+        user.setPassword("pass1234");
+
+        userRepository.save(user);
+        userRepository.delete(user);
+
+        assertTrue(userRepository.findById(user.getId()).isEmpty());
+        assertThrows(UserNotFoundException.class, () -> userRepository.findById(user.getId()));
+    }
+}
